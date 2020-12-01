33b0b4e2d909051b8007c079c06239d4fa21dc95
Emiel de Jong <emieldejong@xs4all.nl>
2020-12-01 15:59:02 +0100

    Links and Switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { NavLink, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import { Home } from './pages/Home';
+import { Blog } from './pages/Blog';
+import { BlogPost } from './pages/BlogPost';
+import { Login } from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      {/* Hier komt de magie ✨✨✨✨ */}
+      <Router>
+        <header>
+          <NavLink exact to="/" activeClassName="active-link" >Home</NavLink>
+          <NavLink to="/blog" activeClassName="active-link" >Blog</NavLink>
+          <NavLink to="/blog-post" activeClassName="active-link" >Blog Post</NavLink>
+          <NavLink to="/login" activeClassName="active-link" >Login</NavLink>
+        </header>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="/blog-post">
+            <BlogPost />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
